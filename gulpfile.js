1010781c69b937559ff7befbb325f3bb2a755875
argha-mirinfo <argha@mirinfosys.com>
2022-12-01 10:56:58 +0600

    project initiallized

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const sourcemaps = require("gulp-sourcemaps");
+
+function style() {
+  return gulp
+    .src("./scss/**/*.scss")
+    .pipe(sourcemaps.init())
+    .pipe(sass({ outputStyle: "compressed" }))
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("./css"));
+}
+
+function watch() {
+  gulp.watch("./scss/**/*.scss", style);
+}
+
+exports.style = style;
+exports.watch = watch;
